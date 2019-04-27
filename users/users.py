012d5e272d483a9ddf22f2dbd0abaa769bc9b9e1
David Dubois <dexcelstraun7@gmail.com>
2019-04-26 20:04:08 -0700

    Adding more things, and also save requirements

---
+++ users/users.py
@@ -0,0 +1,15 @@
+"""Basic API for user-related operations
+"""
+
+def add_user():
+    """Add a new user to the database
+    """
+
+def activate_user():
+
+
+def edit_user():
+
+
+def delete_user():
+
---
+++ util/connection.py
@@ -0,0 +1,17 @@
+import os
+import psycopg2
+import psycopg2.extensions
+
+
+def connect_to_database():
+    db_user = os.environ['DB_USER']
+    db_pass = os.environ['DB_PASS']
+
+    available_port = 5432
+
+    conn = psycopg2.connect('postgresql://{db_user}:{db_pass}@localhost:{available_port}/shaderrepo')
+
+    return conn
+
+
+connection = connect_to_database()
